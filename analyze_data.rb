e56b27a0c4b7a21203b79b7268053899a74d6849
Alexander Rykhlitskiy <alexander.rykhlitskiy@gmail.com>
2018-03-29 12:14:39 +0300

    Use median together with average

--- analyze_data.rb
+++ analyze_data.rb
@@ -1,10 +1,12 @@
 require 'json'
+require_relative 'utils'
 
 stats = JSON.parse(File.read('stats.json'), symbolize_names: true)
 
 def analyze(stats, gender)
-  willpower = stats.values.map { |v| v[gender][:willpower] }.compact
-  puts "#{gender.to_s.capitalize} average willpower: #{willpower.sum / willpower.length}"
+  willpowers = stats.values.map { |v| v[gender][:willpower] if v[gender][:members_count] > 20 }.compact
+  puts "#{gender.to_s.capitalize} average willpower: #{willpowers.sum / willpowers.length}"
+  puts "#{gender.to_s.capitalize} median willpower: #{median(willpowers)}"
 end
 
 analyze(stats, :men)
