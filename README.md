7f39076d3de1eb2633779d6c52ade3d263def1cc
Alexander Rykhlitskiy <alexander.rykhlitskiy@gmail.com>
2018-03-29 15:53:49 +0300

    Prettify output, add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+## Usage
+
+Ruby 2.4 required
+
+```ruby
+ruby collect_data.rb
+ruby analyze_data.rb
+```
+#### Output
+```
+Analyzed 320 weightlifting events.
+Analyzing men
+  (members with fails) / members: 35572 / 49532 = 0.7181619962852297
+  (failed tries) / tries: 51927 / 148596 = 0.34945086005006865
+Analyzing women
+  (members with fails) / members: 23036 / 32834 = 0.7015898154352196
+  (failed tries) / tries: 32478 / 98502 = 0.3297191935189133
+```
--- analyze_data.rb
+++ analyze_data.rb
@@ -4,14 +4,18 @@ require_relative 'utils'
 stats = JSON.parse(File.read('stats.json'), symbolize_names: true)
 
 def analyze(stats, gender)
+  puts "Analyzing #{gender}"
   stats = stats.values.map { |v| v[gender] if v[gender][:members_count] > 20 }.compact
 
-  puts "#{gender.to_s.capitalize} (members with fails) / members: #{stats.map { |v| v[:members_with_fails_count] }.sum / stats.map { |v| v[:members_count] }.sum.to_f}"
+  members_with_fails_count = stats.map { |v| v[:members_with_fails_count] }.sum
+  members_count = stats.map { |v| v[:members_count] }.sum
+  puts "  (members with fails) / members: #{members_with_fails_count} / #{members_count} = #{members_with_fails_count / members_count.to_f}"
 
   fails_count = stats.map { |v| v[:fails_count] }.sum
-  tries_count = (stats.map { |v| v[:members_count] }.sum * 3).to_f
-  puts "#{gender.to_s.capitalize} (failed tries) / tries: #{fails_count / tries_count}"
+  tries_count = (stats.map { |v| v[:members_count] }.sum * 3)
+  puts "  (failed tries) / tries: #{fails_count} / #{tries_count} = #{fails_count / tries_count.to_f}"
 end
 
+puts "Analyzed #{stats.keys.length} weightlifting events."
 analyze(stats, :men)
 analyze(stats, :women)
